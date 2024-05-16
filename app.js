affcd7c8561c2fddbc75145adbbd1e96bf3010d4
roblebob <robert.koppisch@gmail.com>
2024-05-16 14:31:54 +0200

    follow lesson instruction

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require("dotenv/config");
 require("./db");
 const express = require("express");
 
+const { isAuthenticated } = require("./middleware/jwt.middleware");
+
+
 const app = express();
 
 require("./config")(app);
@@ -11,10 +14,14 @@ const allRoutes = require("./routes");
 app.use("/api", allRoutes);
 
 const projectRouter = require("./routes/project.routes");
-app.use("/api", projectRouter);
+app.use("/api", isAuthenticated, projectRouter);
 
 const taskRouter = require("./routes/task.routes");
-app.use("/api", taskRouter);
+app.use("/api", isAuthenticated, taskRouter);
+
+const authRouter = require("./routes/auth.routes");
+app.use("/auth", authRouter);
+
 
 require("./error-handling")(app);
 
